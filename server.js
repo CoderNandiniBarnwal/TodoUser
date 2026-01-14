c47ec68eb465ad0b1f44d87bbc819914e7f7d57b
Nandini Barnwal <tanunandini0@gmail.com>
2026-01-14 17:02:31 +0530

    Complete backend

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv/config";
 import { dbConnect } from "./src/config/dbConnect.js";
 import userRoute from "./src/route/userRoute.js";
 import todoRoute from "./src/route/todoRoute.js";
+import multerRoute from "./src/route/multerRoute.js";
 
 const app = express();
 const port = process.env.PORT;
@@ -10,8 +11,10 @@ const port = process.env.PORT;
 dbConnect();
 
 app.use(express.json());
+app.use('/upload', express.static("upload"))
 app.use("/user", userRoute);
 app.use("/todo", todoRoute);
+app.use("/picture", multerRoute);
 
 app.listen(port, () => {
   console.log(`Server running at Port ${port}`);
